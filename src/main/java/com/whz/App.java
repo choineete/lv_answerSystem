f80d4a02efcb3db2a5647b4a349d06c9a70bd885
LENOVO-PC <237179378@qq.com>
2021-07-18 16:58:27 +0800

    修改测试

--- src/main/java/com/whz/App.java
+++ src/main/java/com/whz/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "xyz" );
+        System.out.println("实验");
     }
 }
