039b49a355b3ff650169daf5b2a51541adcfbcb9
choineete <choineete@163.com>
2021-07-18 20:56:53 +0800

    最初版本1.00

--- src/main/java/com/example/demo/dao/TestDao.java
+++ src/main/java/com/example/demo/dao/TestDao.java
@@ -7,13 +7,22 @@ import java.util.List;
 
 public interface TestDao {
     Single_Choice single_choiceById(@Param("id") Integer id);
+
     List<Single_Choice> single_choice();
+
     int single_choiceAdd(Single_Choice single_choice);
+
     Single_Choice single_choice_wById(@Param("id") Integer id);
+
     List<Single_Choice> single_choice_w();
+
     int single_choiceDelete(@Param("id") Integer id);
+
     int s_w_count();
+
     Single_Choice single_choice_cById(@Param("id") Integer id);
+
     int single_choice_cDelete(@Param("id") Integer id);
+
     int single_choice_cAdd(Single_Choice single_choice);
 }
